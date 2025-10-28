f83148aee2b3051e3c749e59c0041b007c4f46b0
Josselin <julien.josselin75@hotmail.fr>
2025-10-28 09:19:33 +0100

    starters: create j2-netflex-starter

---
+++ starters/j2-netflex-starter/script.js
@@ -0,0 +1,12 @@
+// Configuration TMDB
+const API_KEY = 'VOTRE_CLE_ICI';  // ⚠️ Remplacez par votre clé !
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+// TODO : Sélectionner les éléments du DOM
+
+// TODO : Créer la fonction searchMovies(query)
+
+// TODO : Créer la fonction displayMovies(movies)
+
+// TODO : Ajouter l'event listener sur le formulaire
